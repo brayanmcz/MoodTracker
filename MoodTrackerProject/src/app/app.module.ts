3e4ad15d372a39d9e2ea9888b2488a63860bab11
Brayanmcz <brayan.mcz@gmail.com>
2018-04-06 20:07:33 -0700

    Added login methods

--- MoodTrackerProject/src/app/app.module.ts
+++ MoodTrackerProject/src/app/app.module.ts
@@ -1,18 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-
+import { AuthMethods,
+         AuthProvider,
+         AuthProviderWithCustomConfig,
+         CredentialHelper,
+         FirebaseUIAuthConfig,
+         FirebaseUIModule
+       } from 'firebaseui-angular';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { environment } from './../environments/environment';
+
+const facebookCustomConfig: AuthProviderWithCustomConfig = {
+  provider: AuthProvider.Facebook,
+  customConfig: {
+    scopes: [
+      'public_profile',
+      'email',
+      'user_likes',
+      'user_friends'
+    ],
+    customParameters: {
+      // Forces password re-entry.
+      auth_type: 'reauthenticate'
+    }
+  }
+};
+
+const firebaseUiAuthConfig: FirebaseUIAuthConfig = {
+  providers: [
+    AuthProvider.Google,
+    facebookCustomConfig,
+    AuthProvider.Twitter,
+    AuthProvider.Password,
+  ],
+  method: AuthMethods.Popup,
+  tos: 'https://google.com',
+  credentialHelper: CredentialHelper.AccountChooser,
+  signInSuccessUrl: "./main"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
