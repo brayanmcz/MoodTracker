5ef5e5fbb64b0eec41b5dcabee6424ac7781b6d7
Brayanmcz <brayan.mcz@gmail.com>
2018-04-07 15:35:42 -0700

    Added verification of data

--- MoodTrackerProject/src/app/verify-page/verify-page.component.ts
+++ MoodTrackerProject/src/app/verify-page/verify-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {FirebaseUISignInSuccess} from 'firebaseui-angular';
 
 @Component({
   selector: 'app-verify-page',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verify-page.component.css']
 })
 export class VerifyPageComponent implements OnInit {
+  user: {
+    firstName: string,
+    lastName: string,
+    email: string,
+    birthday: string,
+  }
 
-  constructor() { }
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.user = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      birthday: "",
+    }
+
+    this.afAuth.authState.subscribe(user => {
+      console.log(user);
+
+      this.user = {
+        firstName: user.displayName.split(' ')[0],
+        lastName: user.displayName.split(' ')[1],
+        email: user.email,
+        birthday: "",
+      }
+    });
+  }
 
   ngOnInit() {
+
   }
+
+  clearForm(){
+    this.user = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      birthday: "",
+    }
+    console.log("clicked");
+  }
+
+  logout() {
+    //LOG USER OUT
+    this.afAuth.auth.signOut();
+  }
+
+  successCallback(data: FirebaseUISignInSuccess) {
+    console.log(data);
+  }
+
+
 
 }
