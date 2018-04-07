f3d4166b2e96bc2ce93ac8c9d1fede682f03d2e7
Brayanmcz <brayan.mcz@gmail.com>
2018-04-06 19:06:45 -0700

    Added new routes

--- MoodTrackerProject/src/app/app-routing.module.ts
+++ MoodTrackerProject/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+// TODO: TO DEVELOPER
+//       When adding new components to the RouterModule
+//       make sure to import the component below and then
+//       add it to the routes definition below.
+
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'main', component: MainPageComponent }
+]
 
 @NgModule({
-  imports: [
-    CommonModule
+  exports: [
+    RouterModule
   ],
-  declarations: []
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
 })
 export class AppRoutingModule { }
