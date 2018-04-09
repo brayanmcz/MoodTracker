8c6a1758818e77f3947d7bf6e66c0b27c07b63c0
Brayanmcz <brayan.mcz@gmail.com>
2018-04-08 17:28:47 -0700

    Added webcam and image capture

--- MoodTrackerProject/src/app/main-page/main-page.component.ts
+++ MoodTrackerProject/src/app/main-page/main-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+import {WebcamImage} from "../modules/webcam/domain/webcam-image";
+
 
 @Component({
-  selector: 'app-main-page',
+  selector: 'app-root',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
+  // toggle webcam on/off
+  public showWebcam = true;
 
-  constructor() { }
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
 
-  ngOnInit() {
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
   }
 
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info("received webcam image", webcamImage);
+    this.webcamImage = webcamImage;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
 }
