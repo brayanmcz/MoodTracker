37a99499aa4de652fcd75a04870008e39c8b149d
Brayanmcz <brayan.mcz@gmail.com>
2018-04-08 17:26:27 -0700

    Fixed styling and functionality issues

--- MoodTrackerProject/src/app/verify-page/verify-page.component.ts
+++ MoodTrackerProject/src/app/verify-page/verify-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {FirebaseUISignInSuccess} from 'firebaseui-angular';
+import { AngularFireDatabase } from 'angularfire2/database'
+
 
 @Component({
   selector: 'app-verify-page',
@@ -12,31 +14,55 @@ export class VerifyPageComponent implements OnInit {
     firstName: string,
     lastName: string,
     email: string,
-    birthday: string,
+    birthday: any,
+    verified: boolean,
+    id: string
   }
 
+  firebaseRef: any;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, db: AngularFireDatabase) {
+
+    this.firebaseRef = db;
+
     this.user = {
       firstName: "",
       lastName: "",
       email: "",
       birthday: "",
+      verified: false,
+      id: ""
     }
 
     this.afAuth.authState.subscribe(user => {
-      console.log(user);
+       db.object('/users/' + user.uid + '/verified')
+        .valueChanges()
+        .subscribe(info => {
+          this.user = {
+            firstName: user.displayName.split(' ')[0],
+            lastName: user.displayName.split(' ')[1],
+            email: user.email,
+            birthday: "",
+            verified: false,
+            id: user.uid
+          }
 
-      this.user = {
-        firstName: user.displayName.split(' ')[0],
-        lastName: user.displayName.split(' ')[1],
-        email: user.email,
-        birthday: "",
-      }
+          if (info === null){
+            console.log("info" + info);
+            this.createUserAccountOnFirebase(db, user.uid);
+
+          } else if (info === false){
+            console.log("info: " + info);
+          } else if (info === true){
+            console.log("info: " + info);
+            location.replace("./main");
+          };
+        });
     });
   }
 
   ngOnInit() {
+    // TODO: Create firebase user with info
 
   }
 
@@ -46,6 +72,8 @@ export class VerifyPageComponent implements OnInit {
       lastName: "",
       email: "",
       birthday: "",
+      verified: false,
+      id: ""
     }
     console.log("clicked");
   }
@@ -59,6 +87,15 @@ export class VerifyPageComponent implements OnInit {
     console.log(data);
   }
 
+  createUserAccountOnFirebase(db: AngularFireDatabase, userID: string) {
+    db.object('/users/' + userID).set(this.user);
+  }
 
+  updateUserAccountOnFirebase(db: AngularFireDatabase) {
+    this.firebaseRef.object('/users/' + this.user.id + '/birthday').set(this.user.birthday);
+    this.firebaseRef.object('/users/' + this.user.id + '/verified').set(true);
+    location.replace("./main");
+    // TODO: CHANGE TO NOT LOOK LIKE SHIT
+  }
 
 }
