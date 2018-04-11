8bde6e1fcae0ff69fc6eec09646d592a23773165
Brayanmcz <brayan.mcz@gmail.com>
2018-04-10 17:32:56 -0700

    Changed styling, added show face data

--- MoodTrackerProject/src/app/main-page/main-page.component.ts
+++ MoodTrackerProject/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 import {Observable} from 'rxjs/Observable';
-import {WebcamImage} from "../modules/webcam/domain/webcam-image";
+import {WebcamImage} from "./webcam-image";
 import * as $ from 'jquery';
 import { environment } from './../../environments/environment';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 
 @Component({
@@ -23,17 +24,26 @@ export class MainPageComponent {
   // webcam snapshot trigger
   private trigger: Subject<void> = new Subject<void>();
 
+  // Show/Hide Json text
+  public showJSONBool: boolean = false;
+
+  public showJSON() {
+    this.showJSONBool = !this.showJSONBool;
+  }
+
+  //Called when Take Snapshot button is pressed
   public triggerSnapshot(): void {
     this.trigger.next();
     this.processImage();
   }
 
+  //Called when toggleWebcam button is pressed
   public toggleWebcam(): void {
     this.showWebcam = !this.showWebcam;
   }
 
   public handleImage(webcamImage: WebcamImage): void {
-    console.info("received webcam image", webcamImage);
+    //console.info("received webcam image", webcamImage);
     this.webcamImage = webcamImage;
   }
 
@@ -41,6 +51,7 @@ export class MainPageComponent {
     return this.trigger.asObservable();
   }
 
+  //Called when Take Snapshot button is pressed
   public processImage() {
 
     let params = {
@@ -63,7 +74,66 @@ export class MainPageComponent {
       })
       .done(function(data) {
         $("#responseTextArea").text(JSON.stringify(data[0]));
-        console.log(data[0]);
+        let face = data[0].faceAttributes;
+
+        $("#gender").text(JSON.stringify(face.gender).replace("\"", "").replace("\"", ""));
+        $("#age").text(JSON.stringify(face.age).replace("\"", "").replace("\"", ""));
+
+        if (face.glasses = "NoGlasses"){
+          $("#glasses").text("Nope");
+        } else {
+          $("#glasses").text("Yep");
+        }
+
+        let anger = face.emotion.anger;
+        let contempt = face.emotion.contempt;
+        let disgust = face.emotion.disgust;
+        let fear = face.emotion.fear;
+        let happiness = face.emotion.happiness;
+        let neutral = face.emotion.neutral;
+        let sadness = face.emotion.sadness;
+        let surprise = face.emotion.surprise;
+
+        //// TODO: Complete hack, fix later to look better
+        if(anger>contempt && anger>disgust && anger>fear && anger>happiness && anger>neutral && anger>sadness && anger>surprise){
+          $("#emotion").text("Anger");
+        } else if (contempt>disgust && contempt>fear && contempt>happiness && contempt>neutral && contempt>sadness && contempt>surprise){
+          $("#emotion").text("Contempt");
+        } else if (disgust>fear && disgust>happiness && disgust>neutral && disgust>sadness && disgust>surprise){
+          $("#emotion").text("Disgust");
+        } else if (fear>happiness && fear>neutral && fear>sadness && fear>surprise){
+          $("#emotion").text("Fear");
+        } else if (happiness>neutral && happiness>sadness && happiness>surprise){
+          $("#emotion").text("Happiness");
+        } else if (neutral>sadness && neutral>surprise){
+          $("#emotion").text("Neutral");
+        } else if (sadness >surprise){
+          $("#emotion").text("Sadness");
+        } else {
+          $("#emotion").text("Surprise");
+        }
+
+        let black = face.hair.hairColor[0].confidence;
+        let brown = face.hair.hairColor[1].confidence;
+        let other = face.hair.hairColor[2].confidence;
+        let gray = face.hair.hairColor[3].confidence;
+        let red = face.hair.hairColor[4].confidence;
+        let blond = face.hair.hairColor[5].confidence;
+
+        if (black>brown && black>other && black>gray && black>red && black>blond){
+          $("#hairColor").text("Black");
+        } else if (brown>other && brown>gray && brown>red && brown>blond){
+          $("#hairColor").text("Brown");
+        } else if (other> gray && other>red && other>blond){
+          $("#hairColor").text("Other");
+        } else if (gray>red && gray>blond){
+          $("#hairColor").text("Gray");
+        } else if (red>blond){
+          $("#hairColor").text("Red");
+        } else {
+          $("#hairColor").text("Blond");
+        }
+
       })
       .fail(function(err) {
         $("#responseTextArea").text(JSON.stringify(err));
@@ -71,5 +141,4 @@ export class MainPageComponent {
       })
     });
   }
-
 }
