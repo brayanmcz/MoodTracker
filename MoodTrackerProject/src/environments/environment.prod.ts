d48de45c3161ab113bd1a81fbfb56463ac462c1a
Brayanmcz <brayan.mcz@gmail.com>
2018-04-10 17:37:11 -0700

    Note to developers

--- MoodTrackerProject/src/environments/environment.prod.ts
+++ MoodTrackerProject/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+// TODO: DEVELOPER: get firebase information from firebase console
+//                : get azureAPIKey from azure project
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "<>",
+    authDomain: "<>",
+    databaseURL: "<>",
+    projectId: "<>",
+    storageBucket: "",
+    messagingSenderId: "<>"
+  },
+  azureAPIKey: "<>"
 };
